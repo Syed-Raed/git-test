54592ac2f1e4dc9e13ff5f1ead8329afbd6bc0b7
DG443 <syedraed333@gmail.com>
2023-08-17 21:09:40 +0800

    webpack output management complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from "lodash";
+import printMe from "./print";
 
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    const btn = document.createElement('button')
+
+    element.textContent = _.join(['Hello','webpack'])
+
+    btn.textContent = 'Click me and check the console'
+    btn.onclick = printMe
+
+    element.appendChild(btn)
   
     return element;
   }
